kolyshkin/crun

commit c954b1b64e1ad892f0308b5b552bdb823174cdcb

criu: use a process to initialize the cgroup
under systemd, use a proxy process that initializes the cgroup, which
is especially needed with systemd, and then restore the container at
the already created cgroup.

Closes: https://github.com/containers/crun/issues/1651

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

src/restore.c
@@ -187,6 +187,8 @@ crun_command_restore (struct crun_global_arguments *global_args, int argc, char 
   if (UNLIKELY (ret < 0))
     return ret;
 
+  cr_options.manage_cgroups_mode = -1;
+
   if (cr_options.image_path == NULL)
     {
       cleanup_free char *path = NULL;

src/libcrun/cgroup-internal.h
@@ -56,7 +56,6 @@ int enter_cgroup_subsystem (pid_t pid, const char *subsystem, const char *path, 
                             libcrun_error_t *err);
 int enable_controllers (const char *path, libcrun_error_t *err);
 int chown_cgroups (const char *path, uid_t uid, gid_t gid, libcrun_error_t *err);
-int destroy_cgroup_path (const char *path, int mode, libcrun_error_t *err);
 int cgroup_killall_path (const char *path, int signal, libcrun_error_t *err);
 
 int libcrun_cgroup_read_pids_from_path (const char *path, bool recurse, pid_t **pids, libcrun_error_t *err);

src/libcrun/cgroup-utils.c
@@ -944,7 +944,7 @@ enable_controllers (const char *path, libcrun_error_t *err)
 }
 
 int
-libcrun_move_process_to_cgroup (pid_t pid, pid_t init_pid, char *path, libcrun_error_t *err)
+libcrun_move_process_to_cgroup (pid_t pid, pid_t init_pid, const char *path, bool create_if_missing, libcrun_error_t *err)
 {
   int cgroup_mode = libcrun_get_cgroup_mode (err);
   if (UNLIKELY (cgroup_mode < 0))
@@ -953,7 +953,7 @@ libcrun_move_process_to_cgroup (pid_t pid, pid_t init_pid, char *path, libcrun_e
   if (path == NULL || *path == '\0')
     return 0;
 
-  return enter_cgroup (cgroup_mode, pid, init_pid, path, false, err);
+  return enter_cgroup (cgroup_mode, pid, init_pid, path, create_if_missing, err);
 }
 
 int
@@ -986,4 +986,77 @@ libcrun_get_cgroup_dirfd (struct libcrun_cgroup_status *status, const char *sub_
     return crun_make_error (err, errno, "open `%s`", path_to_cgroup);
 
   return cgroupdirfd;
+}
+
+int
+libcrun_migrate_all_pids_to_cgroup (pid_t init_pid, char *from, char *to, libcrun_error_t *err)
+{
+  cleanup_free pid_t *pids = NULL;
+  cleanup_close int child_dfd = -1;
+  int cgroup_mode;
+  size_t from_len;
+  size_t i;
+  int ret;
+
+  cgroup_mode = libcrun_get_cgroup_mode (err);
+  if (cgroup_mode < 0)
+    return cgroup_mode;
+
+  ret = libcrun_cgroup_pause_unpause_path (from, true, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  ret = libcrun_cgroup_read_pids_from_path (from, true, &pids, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  from_len = strlen (from);
+
+  for (i = 0; pids && pids[i]; i++)
+    {
+      cleanup_free char *pid_path = NULL;
+      cleanup_free char *dest_cgroup = NULL;
+
+      ret = libcrun_get_cgroup_process (pids[i], &pid_path, false, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+
+      /* Make sure the pid is in the cgroup we are migrating from.  */
+      if (! has_prefix (pid_path, from))
+        return crun_make_error (err, 0, "error migrating pid %d.  It is not in the cgroup `%s`", pids[i], from);
+
+      /* Build the destination cgroup path, keeping the same hierarchy.  */
+      xasprintf (&dest_cgroup, "%s%s", to, pid_path + from_len);
+
+      ret = enter_cgroup (cgroup_mode, pids[i], init_pid, dest_cgroup, false, err);
+      if (UNLIKELY (ret < 0))
+        return ret;
+    }
+
+  ret = libcrun_cgroup_pause_unpause_path (from, false, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  return destroy_cgroup_path (from, cgroup_mode, err);
+}
+
+int
+get_cgroup_dirfd_path (int dirfd, char **path, libcrun_error_t *err)
+{
+  cleanup_free char *cgroup_path = NULL;
+  proc_fd_path_t fd_path;
+  ssize_t len;
+
+  get_proc_self_fd_path (fd_path, dirfd);
+
+  len = safe_readlinkat (AT_FDCWD, fd_path, &cgroup_path, 0, err);
+  if (UNLIKELY (len < 0))
+    return len;
+
+  if (has_prefix (cgroup_path, CGROUP_ROOT))
+    {
+      *path = xstrdup (cgroup_path + strlen (CGROUP_ROOT));
+      return 0;
+    }
+  return crun_make_error (err, 0, "invalid cgroup path `%s`", cgroup_path);
 }

src/libcrun/cgroup-utils.h
@@ -22,7 +22,7 @@
 #include "cgroup.h"
 #include <unistd.h>
 
-int libcrun_move_process_to_cgroup (pid_t pid, pid_t init_pid, char *path, libcrun_error_t *err);
+int libcrun_move_process_to_cgroup (pid_t pid, pid_t init_pid, const char *path, bool create_if_missing, libcrun_error_t *err);
 
 int libcrun_cgroups_create_symlinks (int dirfd, libcrun_error_t *err);
 
@@ -33,5 +33,11 @@ int libcrun_get_cgroup_mode (libcrun_error_t *err);
 int libcrun_get_cgroup_dirfd (struct libcrun_cgroup_status *status, const char *sub_cgroup, libcrun_error_t *err);
 
 int maybe_make_cgroup_threaded (const char *path, libcrun_error_t *err);
+
+int libcrun_migrate_all_pids_to_cgroup (pid_t init_pid, char *from, char *to, libcrun_error_t *err);
+
+int destroy_cgroup_path (const char *path, int mode, libcrun_error_t *err);
+
+int get_cgroup_dirfd_path (int dirfd, char **path, libcrun_error_t *err);
 
 #endif

src/libcrun/container.c
@@ -55,6 +55,7 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <grp.h>
+#include <libgen.h>
 #include <git-version.h>
 
 #ifdef HAVE_SYSTEMD
@@ -1925,6 +1926,25 @@ reap_subprocesses (pid_t main_process, int *main_process_exit, int *last_process
 }
 
 static int
+send_sd_notify (const char *ready_str, libcrun_error_t *err)
+{
+#ifdef HAVE_SYSTEMD
+  int ret = sd_notify (0, ready_str);
+  if (UNLIKELY (ret < 0))
+    return crun_make_error (err, -ret, "sd_notify");
+
+#  if HAVE_SD_NOTIFY_BARRIER
+  /* Hard-code a 30 seconds timeout.  Ignore errors.  */
+  sd_notify_barrier (0, 30 * 1000000);
+#  endif
+#else
+  (void) ready_str;
+  (void) err;
+#endif
+  return 0;
+}
+
+static int
 handle_notify_socket (int notify_socketfd, libcrun_error_t *err)
 {
 #ifdef HAVE_SYSTEMD
@@ -1939,14 +1959,9 @@ handle_notify_socket (int notify_socketfd, libcrun_error_t *err)
   buf[ret] = '\0';
   if (strstr (buf, ready_str))
     {
-      ret = sd_notify (0, ready_str);
+      ret = send_sd_notify (ready_str, err);
       if (UNLIKELY (ret < 0))
-        return crun_make_error (err, -ret, "sd_notify");
-
-#  if HAVE_SD_NOTIFY_BARRIER
-      /* Hard-code a 30 seconds timeout.  Ignore errors.  */
-      sd_notify_barrier (0, 30 * 1000000);
-#  endif
+        return ret;
 
       return 1;
     }
@@ -4248,14 +4263,67 @@ libcrun_container_checkpoint (libcrun_context_t *context, const char *id, libcru
   return 0;
 }
 
+static int
+restore_proxy_process (int *proxy_pid_pipe, int cgroup_manager, libcrun_error_t *err)
+{
+  cleanup_free char *own_cgroup_copy = NULL;
+  cleanup_free char *own_cgroup = NULL;
+  const char *parent_cgroup;
+  pid_t new_pid;
+  int mode;
+  int ret;
+
+  ret = TEMP_FAILURE_RETRY (read (*proxy_pid_pipe, &new_pid, sizeof (new_pid)));
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  close_and_reset (proxy_pid_pipe);
+
+  if (cgroup_manager == CGROUP_MANAGER_SYSTEMD)
+    {
+      char ready_str[64];
+
+      sprintf (ready_str, "MAINPID=%d", new_pid);
+      ret = send_sd_notify (ready_str, err);
+      /* Do not fail on errors.  */
+      if (UNLIKELY (ret < 0))
+        crun_error_release (err);
+    }
+
+  ret = libcrun_get_cgroup_process (0, &own_cgroup, false, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  own_cgroup_copy = xstrdup (own_cgroup);
+  parent_cgroup = dirname (own_cgroup_copy);
+
+  ret = libcrun_move_process_to_cgroup (0, 0, parent_cgroup, false, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  mode = libcrun_get_cgroup_mode (err);
+  if (UNLIKELY (mode < 0))
+    return mode;
+
+  ret = destroy_cgroup_path (own_cgroup, mode, err);
+  if (UNLIKELY (ret < 0))
+    return ret;
+
+  return 0;
+}
+
 int
 libcrun_container_restore (libcrun_context_t *context, const char *id, libcrun_checkpoint_restore_t *cr_options,
                            libcrun_error_t *err)
 {
   cleanup_cgroup_status struct libcrun_cgroup_status *cgroup_status = NULL;
   cleanup_container libcrun_container_t *container = NULL;
+  cleanup_close int proxy_pid_pipe0 = -1;
+  cleanup_close int proxy_pid_pipe1 = -1;
   runtime_spec_schema_config_schema *def;
   libcrun_container_status_t status = {};
+  cleanup_pid pid_t proxy_pid = -1;
+  int proxy_pid_pipe[2];
   int cgroup_manager;
   uid_t root_uid = -1;
   gid_t root_gid = -1;
@@ -4280,23 +4348,13 @@ libcrun_container_restore (libcrun_context_t *context, const char *id, libcrun_c
   if (UNLIKELY (ret < 0))
     return ret;
 
-  /* The CRIU restore code uses bundle and rootfs of status. */
-  status.bundle = (char *) context->bundle;
-  status.rootfs = def->root->path;
-
-  ret = libcrun_container_restore_linux (&status, container, cr_options, err);
-  if (UNLIKELY (ret < 0))
-    return ret;
-
-  /* Now that the process has been restored, moved it into is cgroup again.
-   * The whole cgroup code is copied from libcrun_container_run_internal(). */
-  def = container->container_def;
-
   cgroup_manager = CGROUP_MANAGER_CGROUPFS;
   if (context->systemd_cgroup)
     cgroup_manager = CGROUP_MANAGER_SYSTEMD;
   else if (context->force_no_cgroup)
     cgroup_manager = CGROUP_MANAGER_DISABLED;
+
+  def = container->container_def;
 
   /* If we are root (either on the host or in a namespace),
    * then chown the cgroup to root in the container user namespace. */
@@ -4313,17 +4371,86 @@ libcrun_container_restore (libcrun_context_t *context, const char *id, libcrun_c
     }
 
   {
+    cleanup_close int cgroup_dirfd = -1;
+    bool needs_proxy_process;
     struct libcrun_cgroup_args cg = {
       .resources = def->linux ? def->linux->resources : NULL,
       .annotations = container->annotations,
       .cgroup_path = def->linux ? def->linux->cgroups_path : "",
       .manager = cgroup_manager,
-      .pid = status.pid,
       .root_uid = root_uid,
       .root_gid = root_gid,
       .id = context->id,
       .state_root = context->state_root,
     };
+
+    /* The CRIU restore code uses bundle, rootfs and cgroup_path of status.   The cgroup_path is set later.  */
+    status.bundle = (char *) context->bundle;
+    status.rootfs = def->root->path;
+
+    ret = libcrun_cgroup_preenter (&cg, &cgroup_dirfd, err);
+    if (UNLIKELY (ret < 0))
+      return ret;
+
+    needs_proxy_process = cgroup_dirfd < 0;
+
+    /* If the target cgroup was already created (with cgroupfs), we can restore the container directly there.  */
+    if (! needs_proxy_process)
+      {
+        cleanup_free char *cgroup_path = NULL;
+
+        ret = get_cgroup_dirfd_path (cgroup_dirfd, &cgroup_path, err);
+        if (UNLIKELY (ret < 0))
+          return ret;
+
+        /* Restore the container directly in the desired cgroup.  */
+        status.cgroup_path = cgroup_path;
+
+        ret = libcrun_container_restore_linux (&status, container, cr_options, err);
+        if (UNLIKELY (ret < 0))
+          return ret;
+
+        /* Use the container first process PID to setup the cgroup.  */
+        cg.pid = status.pid;
+      }
+    else
+      {
+        /* When using systemd, we need a process first that must exist before a cgroup is created.
+           Create a dummy process that sits and waits to receive the pid of the restored container.
+           Once the dummy process receives the pid, the dummy process will notify the new pid to systemd.  */
+        ret = pipe2 (proxy_pid_pipe, O_CLOEXEC);
+        if (UNLIKELY (ret < 0))
+          return crun_make_error (err, errno, "pipe");
+
+        /* Only used for auto cleanup.  */
+        proxy_pid_pipe0 = proxy_pid_pipe[0];
+        proxy_pid_pipe1 = proxy_pid_pipe[1];
+
+        proxy_pid = fork ();
+        if (UNLIKELY (proxy_pid < 0))
+          return crun_make_error (err, errno, "fork");
+
+        if (proxy_pid == 0)
+          {
+            close_and_reset (&proxy_pid_pipe1);
+
+            ret = restore_proxy_process (&proxy_pid_pipe0, cgroup_manager, err);
+            if (UNLIKELY (ret < 0))
+              {
+                crun_error_release (err);
+                _exit (EXIT_FAILURE);
+              }
+            _exit (EXIT_SUCCESS);
+          }
+
+        close_and_reset (&proxy_pid_pipe0);
+
+        /* Use the dummy process PID as the PID to setup the cgroup.  */
+        cg.pid = proxy_pid;
+      }
+
+    /* Complete the configuration for the cgroup.  It either contains the fully restored container with cgroupfs,
+       or the dummy process with systemd.  */
 
     ret = libcrun_cgroup_enter (&cg, &cgroup_status, err);
     if (UNLIKELY (ret < 0))
@@ -4332,6 +4459,47 @@ libcrun_container_restore (libcrun_context_t *context, const char *id, libcrun_c
     ret = libcrun_cgroup_enter_finalize (&cg, cgroup_status, err);
     if (UNLIKELY (ret < 0))
       return ret;
+
+    /* When using a dummy process, the container is restored here once the cgroup is configured.  */
+    if (needs_proxy_process)
+      {
+        cleanup_free char *target_cgroup = NULL;
+        cleanup_free char *proxy_cgroup = NULL;
+
+        ret = libcrun_get_cgroup_process (proxy_pid, &target_cgroup, false, err);
+        if (UNLIKELY (ret < 0))
+          return ret;
+
+        /* Move the dummy process to a sub-cgroup.  */
+        ret = append_paths (&proxy_cgroup, err, target_cgroup, ".proxy-crun", NULL);
+        if (UNLIKELY (ret < 0))
+          return ret;
+
+        ret = libcrun_move_process_to_cgroup (proxy_pid, 0, proxy_cgroup, true, err);
+        if (UNLIKELY (ret < 0))
+          return ret;
+
+        /* Restore the container in the same cgroup where the dummy process was.  */
+        status.cgroup_path = target_cgroup;
+
+        ret = libcrun_container_restore_linux (&status, container, cr_options, err);
+        if (UNLIKELY (ret < 0))
+          return ret;
+
+        /* Notify the dummy process of the container PID.  It will move itself to its parent cgroup and
+           destroy the cgroup.  */
+        ret = TEMP_FAILURE_RETRY (write (proxy_pid_pipe1, &status.pid, sizeof (status.pid)));
+        if (UNLIKELY (ret < 0))
+          return crun_make_error (err, errno, "write pid to proxy process");
+        close_and_reset (&proxy_pid_pipe1);
+
+        ret = TEMP_FAILURE_RETRY (waitpid (proxy_pid, NULL, 0));
+        if (ret < 0)
+          return crun_make_error (err, errno, "waitpid");
+
+        /* Do not kill the proxy process prematurely.  */
+        proxy_pid = -1;
+      }
   }
 
   context->detach = cr_options->detach;

src/libcrun/criu.c
@@ -88,6 +88,7 @@ struct libcriu_wrapper_s
   int (*criu_set_parent_images) (const char *path);
   void (*criu_set_pid) (int pid);
   int (*criu_set_root) (const char *root);
+  int (*criu_add_cg_root) (const char *ctrl, const char *path);
   void (*criu_set_shell_job) (bool shell_job);
   void (*criu_set_tcp_established) (bool tcp_established);
   void (*criu_set_track_mem) (bool track_mem);
@@ -181,6 +182,7 @@ load_wrapper (struct libcriu_wrapper_s **wrapper_out, libcrun_error_t *err)
   LOAD_CRIU_FUNCTION (criu_set_parent_images, false);
   LOAD_CRIU_FUNCTION (criu_set_pid, false);
   LOAD_CRIU_FUNCTION (criu_set_root, false);
+  LOAD_CRIU_FUNCTION (criu_add_cg_root, false);
   LOAD_CRIU_FUNCTION (criu_set_shell_job, false);
   LOAD_CRIU_FUNCTION (criu_set_tcp_established, false);
   LOAD_CRIU_FUNCTION (criu_set_track_mem, false);
@@ -1031,6 +1033,19 @@ libcrun_container_restore_linux_criu (libcrun_container_status_t *status, libcru
   libcriu_wrapper->criu_set_tcp_established (cr_options->tcp_established);
   libcriu_wrapper->criu_set_file_locks (cr_options->file_locks);
   libcriu_wrapper->criu_set_orphan_pts_master (true);
+
+  if (status->cgroup_path)
+    {
+      ret = libcriu_wrapper->criu_add_cg_root (NULL, status->cgroup_path);
+      if (UNLIKELY (ret != 0))
+        return crun_make_error (err, 0, "error setting CRIU cgroup root to `%s`", status->cgroup_path);
+    }
+
+  if (cr_options->manage_cgroups_mode == -1)
+    /* Defaulting to CRIU_CG_MODE_SOFT just as runc */
+    libcriu_wrapper->criu_set_manage_cgroups_mode (CRIU_CG_MODE_SOFT);
+  else
+    libcriu_wrapper->criu_set_manage_cgroups_mode (cr_options->manage_cgroups_mode);
   libcriu_wrapper->criu_set_manage_cgroups (true);
 
   if (libcriu_wrapper->criu_set_network_lock && cr_options->network_lock_method > 0)

src/libcrun/linux.c
@@ -5059,13 +5059,13 @@ join_process_parent_helper (libcrun_context_t *context,
           if (UNLIKELY (ret < 0))
             return ret;
 
-          ret = libcrun_move_process_to_cgroup (pid, status->pid, final_cgroup, err);
+          ret = libcrun_move_process_to_cgroup (pid, status->pid, final_cgroup, false, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }
       else
         {
-          ret = libcrun_move_process_to_cgroup (pid, status->pid, status->cgroup_path, err);
+          ret = libcrun_move_process_to_cgroup (pid, status->pid, status->cgroup_path, false, err);
           if (UNLIKELY (ret < 0))
             return ret;
         }

commit e8511d6d88d03d7b3c93ae38297512154acffea7

restore: fix --manage-cgroups-mode
The default value for manage_cgroups_mode is set after the command line
options are parsed, overriding whatever is set by --manage-cgroups-mode.
As a result, crun restore always uses the "soft" mode.

In particular, restoring into a different cgroup with "ignore" mode
fails with something like:

  Error (criu/cgroup.c:1187): cg: Can't move 1 into unified//old/cgroup.procs (-1/-1): No such file or directory
  Error (criu/cgroup.c:1245): cg: couldn't set cgns prefix unified//old/cgroup.procs: No such file or directory
  Error (criu/cgroup.c:1338): cg: failed preparing cgns

Set the default before parsing the options, like crun checkpoint does.

Found by the "checkpoint then restore into a different cgroup (via
--manage-cgroups-mode ignore)" runc integration test.

Fixes: c954b1b6 ("criu: use a process to initialize the cgroup")
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

src/restore.c
@@ -166,6 +166,8 @@ crun_command_restore (struct crun_global_arguments *global_args, int argc, char 
   int first_arg;
   int ret;
 
+  cr_options.manage_cgroups_mode = -1;
+
   argp_parse (&run_argp, argc, argv, ARGP_IN_ORDER, &first_arg, &cr_options);
   crun_assert_n_args (argc - first_arg, 1, 2);
 
@@ -195,8 +197,6 @@ crun_command_restore (struct crun_global_arguments *global_args, int argc, char 
   ret = init_libcrun_context (&crun_context, argv[first_arg], global_args, err);
   if (UNLIKELY (ret < 0))
     return ret;
-
-  cr_options.manage_cgroups_mode = -1;
 
   if (cr_options.image_path == NULL)
     {